c995cadf2f44d3a8e6a1f909aa14c55b395214af
yossef-cohen <yossef187y@gmail.com>
2024-11-30 16:23:20 +0200

    Created dimensionality reduction function still need to be tested

--- main.py
+++ main.py
@@ -28,9 +28,43 @@ def remove_sparse_columns(df: pd.DataFrame, threshold: int) -> pd.DataFrame:
 
 
 def dimensionality_reduction(df: pd.DataFrame, num_components: int, meta_columns: list[str]) -> pd.DataFrame:
-    df_sub_mean = df - df.mean()
-    df_standardized = df_sub_mean / df.std()
-    pass
+    # save the metadata and save it for later use
+    metadata = df[meta_columns]
+
+    # remove the metadat for the df
+    metadata_removed = df.drop(columns=meta_columns)
+
+    # standardize the data shift the data around zero
+    df_standardize = (metadata_removed - metadata.mean()) / metadata.std()
+
+    # center the data around 0 and find the covariance
+    cov_matrix = np.cov(df_standardize.T)
+
+    # find the eigenvalues and eigenvectors
+    eig, eig_matrix = np.linalg.eig(cov_matrix)
+
+    # find the sorted indexes from the large to small
+    sorted_indexes = np.argsort(eig)[::-1]
+
+    # sort by the index
+    sorted_eig_matrix = eig_matrix[:, sorted_indexes]
+
+    # select the top eigenvectors (columns) according to the argument num_components
+    top_eig = sorted_eig_matrix[:, :num_components]
+
+    # project the data into lower dimension plane according to the incorporate of the eigenvectors
+    reduced_data = df_standardize.dot(top_eig)
+
+    # columns name
+    columns_names = [f"PC{i+1}" for i in range(num_components)]
+
+    # rename the columns to pc1, pc2 ...
+    reduced_dg = pd.DataFrame(reduced_data, columns=columns_names)
+
+    # combine the metadata with the reduced dataframe
+    final_df = pd.concat([metadata_removed.reset_index(drop=True), reduced_dg], axis=1)
+
+    return final_df
 
 
 print((group_and_aggregate_data(load_data('knesset_25.xlsx'), 'city_name', min)))
